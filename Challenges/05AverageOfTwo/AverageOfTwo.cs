d18505f025ef38b2dae9d07468de75b0c1421c4e
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-03-24 20:58:40 -0300

    updated algorithm 5 logic

--- Challenges/05AverageOfTwo/AverageOfTwo.cs
+++ Challenges/05AverageOfTwo/AverageOfTwo.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Challenges.05AverageOfTwo
+namespace _100_Algorithms_Csharp.Challenges_05AverageOfTwo
 {
     public class AverageOfTwo
     {
-        
+        public static void Main(string[] args)
+        {
+             Algorithm();
+             PressEnterToExit();
+        }
+
+         public static void Algorithm()
+         {
+             Console.WriteLine("Enter the grade of the first test:");
+             double grade1 = double.Parse(Console.ReadLine()!);
+
+             Console.WriteLine("Enter the grade of the second test:");
+             double grade2 = double.Parse(Console.ReadLine()!);
+
+             double average = (grade1 + grade2) / 2;
+
+             Console.WriteLine($"The average between {grade1} and {grade2} is {average}");
+         }
+
+
+         public static void PressEnterToExit() 
+         {
+             Console.WriteLine("Press any key to continue...");
+             Console.ReadKey();
+         }
+
     }
 }
