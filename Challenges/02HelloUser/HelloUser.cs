51f21bca01299093f85a210bbfd3d054d1ecadb8
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-03-24 16:08:20 -0300

    updated algorithm 02 statement

--- Challenges/02HelloUser/HelloUser.cs
+++ Challenges/02HelloUser/HelloUser.cs
@@ -1,3 +1,10 @@
+/*
+2) Write a program that reads a person's name and displays a welcome message for them:  
+Example:  
+What is your name? João da Silva  
+Hello João da Silva, it's a pleasure to meet you!
+*/
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
