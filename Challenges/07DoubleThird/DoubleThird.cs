f8056541087a667424b1270904dfb813f3a089d0
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-03-26 15:38:09 -0300

    updated algorithm 7 statement

--- Challenges/07DoubleThird/DoubleThird.cs
+++ Challenges/07DoubleThird/DoubleThird.cs
@@ -1,9 +1,18 @@
+/* 
+7) Create an algorithm that reads a real number and displays its double and its one-third.
+
+Example:
+Enter a number: 3.5
+The double of 3.5 is 7.0
+One-third of 3.5 is 1.16666
+*/
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Challenges.07DoubleThird
+namespace _100_Algorithms_Csharp.Challenges_07DoubleThird
 {
     public class DoubleThird
     {
