ecf4b584230e54d8e0d1ca50cc8dfffb9377c164
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-03-26 15:55:29 -0300

    updated algorithm 09 boilerplate

---
+++ Challenges/09CurrencyConverter/CurrencyConverter.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Challenges.09CurrencyConverter
+{
+    public class CurrencyConverter
+    {
+        
+    }
+}
