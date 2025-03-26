ca7229905f2181342f724b112cf24a7599184fe2
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-03-26 16:13:43 -0300

    updated algorithm 11 statement

--- Challenges/11DeltaCalculator/DeltaCalculator.cs
+++ Challenges/11DeltaCalculator/DeltaCalculator.cs
@@ -1,9 +1,14 @@
+/* 
+ 11) Develop a logic that reads the values of A, B, and C 
+ of a quadratic equation and shows the value of Delta.
+*/
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Challenges.11DeltaCalculator
+namespace _100_Algorithms_Csharp.Challenges_11DeltaCalculator
 {
     public class DeltaCalculator
     {
