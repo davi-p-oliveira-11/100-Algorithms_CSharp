c4334acbe7ffd7e55af9afa05a0b7a3b8a53335a
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-03-26 16:06:37 -0300

    updated algorithm 10 statement

--- Challenges/10PaintNeeded/PaintNeeded.cs
+++ Challenges/10PaintNeeded/PaintNeeded.cs
@@ -1,9 +1,16 @@
+/* 
+ 10) Write an algorithm that reads the width and height of a wall, 
+ calculates and displays the area to be painted, and the amount of paint needed for the job, 
+ knowing that each liter of paint covers an area of 2 square meters.
+*/
+
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Challenges.10PaintNeeded
+namespace _100_Algorithms_Csharp.Challenges_10PaintNeeded
 {
     public class PaintNeeded
     {
