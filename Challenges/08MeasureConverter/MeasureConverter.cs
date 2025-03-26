277e697b180184ce0bbbb548e81fe8e9f8734b07
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-03-26 15:47:49 -0300

    updated algorithm 09 statement

--- Challenges/08MeasureConverter/MeasureConverter.cs
+++ Challenges/08MeasureConverter/MeasureConverter.cs
@@ -1,9 +1,22 @@
+/* 
+9) Develop a program that reads a distance in meters and displays the equivalent values in other units.
+Example:
+Enter a distance in meters: 185.72
+The distance of 185.72m corresponds to:
+0.18572Km
+1.8572Hm
+18.572Dam
+1857.2dm
+18572.0cm
+185720.0mm
+*/
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Challenges.08MeasureConverter
+namespace _100_Algorithms_Csharp.Challenges_08MeasureConverter
 {
     public class MeasureConverter
     {
